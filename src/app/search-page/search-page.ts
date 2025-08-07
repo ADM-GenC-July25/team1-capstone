697578f96a0f0d0fc0643ed1f5cc7688c754604a
Beckers <978358@cognizant.com>
2025-08-07 15:17:12 -0500

    init search-page

--- src/app/search-page/search-page.ts
+++ src/app/search-page/search-page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-search-page',
@@ -8,5 +8,7 @@ import { Component } from '@angular/core';
   styleUrl: './search-page.css'
 })
 export class SearchPage {
-
+  @Input() featuredProducts: any[] = [];
+  @Input() addToCart!: (productId: any) => void;
 }
+
