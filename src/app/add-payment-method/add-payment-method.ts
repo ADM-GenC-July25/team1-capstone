aac0cfa406007ab1f5d236836fba6e7897ee9ff3
eperry910 <eperry910@yahoo.com>
2025-09-02 12:09:30 -0400

    Created component for adding payment method

---
+++ src/app/add-payment-method/add-payment-method.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-payment-method',
+  imports: [],
+  templateUrl: './add-payment-method.html',
+  styleUrl: './add-payment-method.css'
+})
+export class AddPaymentMethod {
+
+}
